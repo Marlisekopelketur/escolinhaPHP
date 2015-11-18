f43f5b323cac8a7d110b15a88ac1c2f11590dcab
Marlise <mpazakopelke@gmail.com>
2015-11-18 20:39:15 -0200

    Criação do arquivo index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Manuseio de Formulário com PHP</h1>
+        <h2>Referências:</h2>
+        <ul>
+            <li><a href="http://www.w3schools.com/php/php_forms.asp">W3Schools</a>></li>
+            <li><a href="http://php.net/">MAnual de PHP</a></li>
+            
+        </ul>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
