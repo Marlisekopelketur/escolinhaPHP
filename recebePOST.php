092ac50cf586005aedb2d4f2849ef814491704c6
Marlise <mpazakopelke@gmail.com>
2015-11-18 21:37:00 -0200

    Manuseio de formulário .ahttp://www.w3schools.com/php/php_forms.asp

---
+++ recebePOST.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Envia dados através de POST</title>
+    </head>
+    <body>
+        <h1>Exemplo de recebimento - Método POST</h1>
+        
+        Seja bem-vindo: <?php echo $_POST["name"]; ?><br>
+        Seu endereço de e-mail é: <?php echo $_POST["email"]; ?>
+        
+    </body>
+</html>
+
