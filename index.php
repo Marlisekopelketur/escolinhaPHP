269590438f4ae5ee61334dc04b193703d394d936
Marlise <mpazakopelke@gmail.com>
2015-11-19 23:41:29 -0200

    http://www.w3schools.com/php/php_form_complete.asp https://github.com/Marlisekopelketur/senaibrusque/issues/41

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $nameErr = $emailErr = $genderErr = $websiteErr = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["name"])) {
-        $nameErr = "Apenas letras e espaços em brancos são permitidos";
+        $nameErr = "Preencher o nome";
     } else {
         $name = test_input($_POST["name"]);
         if (!preg_match("/^[a-zA-Zà-úÀ-Ú ]*$/", $name)) {
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $website = test_input($_POST["website"]);
         if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)) {
-            $websiteErr =  "URL Invalido";
+            $websiteErr = "URL Invalido";
         }
     }
 
@@ -71,32 +71,37 @@ function test_input($data) {
         <h1>Manuseio de Formulário com PHP</h1>
         <h2>Referências:</h2>
         <ul>
-            <li><a href="http://www.w3schools.com/php/php_forms.asp">W3Schools</a>></li>
+            <li><a href="http://www.w3schools.com/php/php_forms.asp">W3Schools</a></li>
             <li><a href="http://php.net/">Manual de PHP</a></li>
 
         </ul>
         <form method="post" 
               action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 
-            Name: <input type="text" name="name" class="<?= strlen($nameErr) != 0 ? "err" : ""; ?>">
+            Name: <input type="text" name="name" class="<?= strlen($nameErr) != 0 ? "err" : ""; ?>" value="<?php echo $name; ?>">
             <span class="error">* <?php echo @$nameErr; ?></span>
             <br><br>
             E-mail:
-            <input type="text" name="email"  class="<?= strlen($emailErr) != 0 ? "err" : ""; ?>">
+            <input type="text" name="email"  class="<?= strlen($emailErr) != 0 ? "err" : ""; ?>" value="<?php echo $email; ?>">
             <span class="error">* <?php echo @$emailErr; ?></span>
             <br><br>
             Website:
-            <input type="text" name="website">
+            <input type="text" 
+                   name="website"
+                   
+                   value="<?php echo $website; ?>">
             <span class="error"><?php echo @$websiteErr; ?></span>
             <br><br>
-            Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+            Comment: <textarea name="comment" rows="5" cols="40"><?= $coment; ?></textarea>
             <br><br>
             Gender:
-            <input type="radio" name="gender" value="female"  class="<?= strlen($nameErr) != 0 ? "err" : ""; ?>">Female
-            <input type="radio" name="gender" value="male"  class="<?= strlen($nameErr) != 0 ? "err" : ""; ?>">Male
+            <input type="radio" name="gender" value="female"  class="<?= strlen($nameErr) != 0 ? "err" : ""; ?>" <?php if (isset($gender) && $gender == "female") echo "checked"; ?>
+                   value="female">Female>Femenino
+            <input type="radio" name="gender" value="male"  class="<?= strlen($nameErr) != 0 ? "err" : ""; ?>" <?php if (isset($gender) && $gender == "female") echo "checked"; ?>
+                   value="female">Female>Masculino
             <span class="error">* <?php echo @$genderErr; ?></span>
             <br><br>
-            <input type="submit" name="submit" value="Submit"> 
+            <input type="submit" name="submit" value="Enviar"> 
 
         </form>
 
